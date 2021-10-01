b55df612531af88c3bbc42bdb130ca1e6cd32056
uvsq22109090 <ghouilamehdi@outlook.fr>
2021-10-01 16:28:19 +0100

    dernier

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,3 +6,27 @@ public class Main {
 	// write your code here
     }
 }
+
+public class Fraction {
+
+    private float a;
+    private float b;
+
+
+    public  Fraction(float a,float b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public String toString(){
+        return "Fraction : "+a+"/"+b ;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getA() {
+        return a;
+    }
+}
